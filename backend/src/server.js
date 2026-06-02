c16ffad29c16ad5a8ea6cd09f057ad72f09184a9
Dylan Stranger <dylan.scheepers@student.fontys.nl>
2026-06-02 18:13:46 +0200

    Server JS add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: "https://yoroomie.azurewebsites.net",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
